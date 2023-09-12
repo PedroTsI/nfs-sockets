07b72648bc0ff9acd5ed12c3361cad7a099ec43a
Pedro Paulo Cantalice Marinho Braga <pedro.cantalice@assert.ifpb.edu.br>
2023-09-12 14:54:52 -0300

    NFS Final

--- src/br/edu/ifpb/gugawag/so/sockets/Client.java
+++ src/br/edu/ifpb/gugawag/so/sockets/Client.java
@@ -1,18 +1,62 @@
 package br.edu.ifpb.gugawag.so.sockets;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import javax.xml.crypto.Data;
 
 public class Client {
 
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 7000);
-        Scanner in = new Scanner(socket.getInputStream());
-        while(in.hasNextLine()) {
-            System.out.println(in.nextLine());
+        private static List<String> opcoes = new ArrayList<>(){
+        {
+            add("Listar Arquivos");
+            add("Renomear arquivo");
+            add("Criar arquivo");
+            add("Excluir arquivo");
+            add("Sair");
         }
-        in.close();
-        socket.close();
+    };
+
+    public static void main(String[] args) throws IOException{
+        Socket socket = new Socket("localhost", 1030);
+
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        
+        System.out.println("Cliente conectado\n");
+
+        String opcao = menu();
+        int opcaoInt = Integer.parseInt(opcao.split(" ")[0]);
+
+        while(opcaoInt < opcoes.size()){
+            dos.writeUTF(opcao);
+
+            if (opcaoInt == 1){
+                System.out.println(dis.readUTF());
+            }
+
+            opcao = menu();
+            opcaoInt = Integer.parseInt(opcao.split(" ")[0]);
+        }
+
+        System.out.println("Cliente desconectado");
+    }
+
+    public static String menu() {
+        System.out.println("\n==== Network File System ====");
+        opcoes.forEach((opcao) -> {
+            System.out.println((opcoes.indexOf(opcao)+1) + " - " + opcao);
+        });
+
+        Scanner entrada = new Scanner(System.in);
+        return entrada.nextLine();
     }
 }
--- src/br/edu/ifpb/gugawag/so/sockets/Server.java
+++ src/br/edu/ifpb/gugawag/so/sockets/Server.java
@@ -1,23 +1,78 @@
 package br.edu.ifpb.gugawag.so.sockets;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Server {
+    private static String FILES_DIRECTORY = System.getProperty("user.dir") + "/files";
+
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(7000);
-        System.out.println("==Servidor==");
-        while(true) {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Cliente " + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
+        ServerSocket serverSocket = new ServerSocket(1030);
+        System.out.println("Server iniciado\n");
 
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-            out.println("Tchau!");
-            out.close();
-            clientSocket.close();
+        Socket socket = serverSocket.accept();
+        System.out.println("Cliente conectado");
+
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+
+        while (true) {
+            String entrada = dis.readUTF();
+
+            int opcao = Integer.parseInt(entrada.split(" ")[0]);
+
+            switch (opcao) {
+                case 1 -> {
+                    List<String> arquivos = readdir();
+                    StringBuilder listagem = new StringBuilder();
+
+                    for (String arquivo : arquivos) {
+                        listagem.append(arquivo).append(" ");
+                    }
+
+                    dos.writeUTF(listagem.toString());
+                }
+                case 2 -> rename(entrada.split(" ")[1], entrada.split(" ")[2]);
+                case 3 -> create(entrada.split(" ")[1]);
+                case 4 -> remove(entrada.split(" ")[1]);
+            }
         }
+    }
 
+    private static List<String> readdir() throws IOException {
+        System.out.println("abc");
+        Stream<Path> files = Files.list(Paths.get(FILES_DIRECTORY));
+        List<String> arquivos = new ArrayList<>();
+        for (Path file : files.toList()) {
+            arquivos.add(file.getFileName().toString());
+        }
+        return arquivos;
+    }
+
+    private static void rename(String nomeAnterior, String nomeNovo) throws IOException {
+        Path file = Paths.get(FILES_DIRECTORY + "/" + nomeAnterior);
+        Files.move(file, file.resolveSibling(nomeNovo));
+    }
+
+    private static void create(String nomeArquivo) throws IOException {
+        Path file = Paths.get(FILES_DIRECTORY + "/" + nomeArquivo);
+        Files.createFile(file);
+    }
+
+    private static void remove(String nomeArquivo) throws IOException {
+        Path file = Paths.get(FILES_DIRECTORY + "/" + nomeArquivo);
+        Files.delete(file);
     }
 }
